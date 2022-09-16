a9fe51245b8cc6b3e34ddd4f824a917e3ba1e09f
arunsankar609 <arunsankar248@gmail.com>
2022-09-16 11:58:06 +0530

    first commit

---
+++ Frontend/src/Pages/client/LandingPage.js
@@ -0,0 +1,75 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+
+// components
+
+ 
+ 
+
+export default function Landing() {
+  return (
+    <>
+     
+      <main>
+        <div className="relative pt-16 pb-32 flex content-center items-center justify-center min-h-screen-75">
+          <div
+            className="absolute top-0 w-full h-full bg-center bg-cover"
+            style={{
+              backgroundImage:
+                "url('https://www.remark-group.co.uk/wp-content/uploads/2019/07/MINI_006_with_screen_original__44146.1506701635.jpg')",
+                height:'100vh'
+            }}
+          >
+            <span
+              id="blackOverlay"
+              className="w-full h-full absolute opacity-75 bg-black"
+            ></span>
+          </div>
+          <div className="container relative mx-auto">
+            <div className="items-center flex flex-wrap">
+              <div className="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center">
+                <div className="pr-12">
+                  <h1 className="text-white font-semibold text-5xl">
+                  We can help you...
+                  </h1>
+                  <p className="text-white mt-4 text-lg text-blueGray-200" >
+                    start your dream with us.we will support you in your journey.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            className="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden h-70-px"
+            style={{ transform: "translateZ(0)" }}
+          >
+            <svg
+              className="absolute bottom-0 overflow-hidden"
+              xmlns="http://www.w3.org/2000/svg"
+              preserveAspectRatio="none"
+              version="1.1"
+              viewBox="0 0 2560 100"
+              x="0"
+              y="0"
+            >
+              <polygon
+                className="text-blueGray-200 fill-current"
+                points="2560 0 2560 100 0 100"
+              ></polygon>
+            </svg>
+          </div>
+        </div>
+
+        
+
+         
+
+       
+
+         
+        
+      </main>
+   
+    </>
+  );
+}
